09c8ab9fa3124700940fec5efa39c684fb84684e
Abdul Mateen <abdulmateenje03@gmail.com>
2026-04-26 12:15:11 +0500

    minor fix docker deployment

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -32,7 +32,12 @@ const options: swaggerJsdoc.Options = {
       },
     ],
   },
-  apis: ['./src/routes/*.ts', './src/controllers/*.ts'], // Path to the API docs
+  apis: [
+    './src/routes/*.ts',
+    './src/controllers/*.ts',
+    './dist/routes/*.js',
+    './dist/controllers/*.js'
+  ], // Path to the API docs
 };
 
 const swaggerSpec = swaggerJsdoc(options);
